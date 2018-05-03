2d85d54e652b12878cf4e030e782cf9f7fca9f33
Ulises Olivenza <ulises.olivenza@schibsted.com>
2018-05-03 15:34:58 +0200

    Add mysql drivers

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -20,6 +23,16 @@ func main() {
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func insert(c echo.Context) error {
+	db, _ := sql.Open("mysql", "dellis:@/shud")
+	defer db.Close()
+
+	// Connect and check the server version
+	var version string
+	db.QueryRow("SELECT VERSION()").Scan(&version)
+	fmt.Println("Connected to:", version)
 }
 
 // Handler
