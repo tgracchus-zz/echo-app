d1fa903d899cdd9349739a8d8cb34780daba4362
Ulises Olivenza <ulises.olivenza@schibsted.com>
2018-05-09 11:06:52 +0200

    Name change

--- repo/db.go
+++ repo/db.go
@@ -3,18 +3,11 @@ package repo
 import (
 	"database/sql"
 
-	"fmt"
-
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/tgracchus/echo-app/config"
 )
 
 func NewDBInstance(cfg config.DatabaseConfig, driver string) (*sql.DB, error) {
-
 	connectionString := cfg.BuildConnectionString()
-
-	fmt.Println(connectionString)
-	//SQL
 	return sql.Open(driver, connectionString)
-
 }
